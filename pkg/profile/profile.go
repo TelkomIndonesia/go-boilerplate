53848ccea01df5ea02b167733e32105b76e71caf
Rucciva <rucciva@gmail.com>
2024-03-15 13:45:56 +0700

    mask string with less than 3

--- pkg/profile/profile.go
+++ pkg/profile/profile.go
@@ -21,31 +21,33 @@ type Profile struct {
 }
 
 func (p Profile) AsLog() any {
+	if len(p.NIN) >= 3 {
+		p.NIN = "***" + p.NIN[len(p.NIN)-3:]
+	} else {
+		p.NIN = "***"
+	}
+
 	if len(p.Name) >= 3 {
-		p.Name = p.Name[:3]
+		p.Name = p.Name[:3] + "***"
+	} else {
+		p.Name = "***"
 	}
 
 	if len(p.Email) >= 3 {
-		p.Email = p.Email[:3]
+		p.Email = p.Email[:3] + "***"
+	} else {
+		p.Email = "***"
 	}
 
 	if len(p.Phone) >= 3 {
-		p.Phone = p.Phone[:3]
+		p.Phone = "***" + p.Phone[len(p.Phone)-3:]
+	} else {
+		p.Phone = "***"
 	}
 
-	if len(p.NIN) >= 3 {
-		p.NIN = p.NIN[len(p.NIN)-3:]
-	}
+	p.DOB = time.Date(p.DOB.Year(), 1, 1, 0, 0, 0, 0, p.DOB.Location())
 
-	return Profile{
-		TenantID: p.TenantID,
-		ID:       p.ID,
-		NIN:      "***" + p.NIN,
-		Name:     p.Name + "***",
-		Email:    p.Email + "***",
-		Phone:    p.Phone + "***",
-		DOB:      time.Date(p.DOB.Year(), 1, 1, 0, 0, 0, 0, p.DOB.Location()),
-	}
+	return p
 }
 
 type ProfileRepository interface {

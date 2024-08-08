500b6ace13f1223ab2f49c4d9485cc3fe87b4dbb
Rucciva <rucciva@gmail.com>
2024-08-08 14:44:28 +0700

    add routing by subject

--- pkg/kafka/outbox.go
+++ pkg/kafka/outbox.go
@@ -3,6 +3,8 @@ package kafka
 import (
 	"context"
 
+	"github.com/IBM/sarama"
+	"github.com/cloudevents/sdk-go/protocol/kafka_sarama/v2"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/cloudevents/sdk-go/v2/event"
 	"github.com/telkomindonesia/go-boilerplate/pkg/util/outboxce"
@@ -11,8 +13,11 @@ import (
 func (k *Kafka) OutboxceRelayFunc() outboxce.RelayFunc {
 	return func(ctx context.Context, events []event.Event) (err error) {
 		idxErr := 0
-		for i, e := range events {
-			result := k.client.Send(ctx, e)
+		for i, event := range events {
+			result := k.client.Send(
+				kafka_sarama.WithMessageKey(context.Background(), sarama.StringEncoder(event.Subject())),
+				event,
+			)
 			if !cloudevents.IsACK(result) {
 				err, idxErr = result, i
 				break

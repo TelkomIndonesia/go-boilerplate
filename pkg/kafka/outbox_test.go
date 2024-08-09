6480e19c41cf331407af67fe71872d25a761737d
Rucciva <rucciva@gmail.com>
2024-08-09 14:28:28 +0700

    refactor test

--- pkg/kafka/outbox_test.go
+++ pkg/kafka/outbox_test.go
@@ -15,62 +15,98 @@ import (
 	"github.com/telkomindonesia/go-boilerplate/pkg/util/outboxce"
 )
 
-var _ cloudevents.Client = mockClient{}
+var _ cloudevents.Client = tclientSendFailed{}
 
-type mockClient struct {
-	t       *testing.T
-	errored map[string]event.Event
+type tclientSendFailed struct {
+	t      *testing.T
+	failed map[string]*event.Event
 }
 
-// Request implements client.Client.
-func (m mockClient) Request(ctx context.Context, event event.Event) (*event.Event, protocol.Result) {
-	panic("unimplemented")
-}
-
-// Send implements client.Client.
-func (m mockClient) Send(ctx context.Context, event event.Event) protocol.Result {
-	_, ok := m.errored[event.ID()]
+func (m tclientSendFailed) Send(ctx context.Context, event event.Event) protocol.Result {
+	e, ok := m.failed[event.ID()]
 	if !ok {
 		return protocol.ResultACK
 	}
-	if rand.Int()%2 == 0 {
-		return protocol.ResultNACK
-	} else {
-		return &protocol.ErrTransportMessageConversion{}
+	if e == nil {
+		m.t.Errorf("event should not be sent due to error on previous event: %v", event)
 	}
+
+	failures := []protocol.Result{
+		protocol.ResultNACK,
+		&protocol.ErrTransportMessageConversion{},
+	}
+	return failures[rand.Int()%len(failures)]
 }
 
-// StartReceiver implements client.Client.
-func (m mockClient) StartReceiver(ctx context.Context, fn interface{}) error {
-	panic("unimplemented")
+func (m tclientSendFailed) Request(ctx context.Context, event event.Event) (*event.Event, protocol.Result) {
+	m.t.Fatal("should not be called")
+	return nil, nil
+}
+
+func (m tclientSendFailed) StartReceiver(ctx context.Context, fn interface{}) error {
+	m.t.Fatal("should not be called")
+	return nil
 }
 
 func TestOutboxcePartialErrors(t *testing.T) {
-	client := mockClient{
-		t:       t,
-		errored: map[string]event.Event{},
+	client := tclientSendFailed{
+		t:      t,
+		failed: map[string]*event.Event{},
 	}
 
-	total, sent := 10, 5
-	events := []event.Event{}
-	for i := 0; i < total; i++ {
-		e := event.New()
-		e.SetID(uuid.NewString())
-		e.SetType(t.Name())
-		if i >= sent {
-			client.errored[e.ID()] = e
-		}
-		events = append(events, e)
+	scenarios := []struct {
+		name  string
+		total int
+		sent  int
+	}{
+		{
+			name:  "NoError",
+			total: 10,
+			sent:  10,
+		},
+		{
+			name:  "PartialError",
+			total: 10,
+			sent:  3,
+		},
+		{
+			name:  "AllError",
+			total: 10,
+			sent:  0,
+		},
 	}
+	for _, d := range scenarios {
+		t.Run(d.name, func(t *testing.T) {
+			events := []event.Event{}
+			for i := 0; i < d.total; i++ {
+				e := event.New()
+				e.SetID(uuid.NewString())
+				e.SetType(t.Name())
+				if i == d.sent {
+					client.failed[e.ID()] = &e
+				}
+				if i > d.sent {
+					client.failed[e.ID()] = nil
+				}
+				events = append(events, e)
+			}
 
-	k := Kafka{client: client}
-	relayErrs := &outboxce.RelayErrors{}
-	err := k.OutboxceRelayFunc()(context.Background(), events)
-	require.Error(t, err)
-	require.True(t, errors.As(err, &relayErrs))
-	assert.Len(t, *relayErrs, total-sent)
-	for _, err := range *relayErrs {
-		_, ok := client.errored[err.Event.ID()]
-		assert.True(t, ok)
+			k := Kafka{client: client}
+			relayErrs := &outboxce.RelayErrors{}
+			err := k.OutboxceRelayFunc()(context.Background(), events)
+			switch d.total == d.sent {
+			case false:
+				require.Error(t, err)
+				require.True(t, errors.As(err, &relayErrs))
+				assert.Len(t, *relayErrs, d.total-d.sent)
+				for _, err := range *relayErrs {
+					_, ok := client.failed[err.Event.ID()]
+					assert.True(t, ok)
+				}
+
+			case true:
+				require.NoError(t, err)
+			}
+		})
 	}
 }

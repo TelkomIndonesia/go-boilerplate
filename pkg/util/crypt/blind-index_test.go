afd5ccb65be7c56b07a9713351daee6a99318722
Rucciva <rucciva@gmail.com>
2024-08-12 22:59:42 +0700

    refactor test

--- pkg/util/crypt/blind-index_test.go
+++ pkg/util/crypt/blind-index_test.go
@@ -10,8 +10,10 @@ import (
 )
 
 func TestBlindIndexes(t *testing.T) {
+	length := 16
+
 	mgr := keyset.NewManager()
-	hid, err := mgr.Add(mac.HMACSHA256Tag256KeyTemplate())
+	hid, err := mgr.Add(mac.HMACSHA256Tag128KeyTemplate())
 	require.NoError(t, err, "should add mac handle")
 	err = mgr.SetPrimary(hid)
 	require.NoError(t, err, "should set primary handle")
@@ -23,28 +25,40 @@ func TestBlindIndexes(t *testing.T) {
 	data := []byte("asdasjdiu9lksdlfkjasopfijaposdpasi09ie283u023hj02i0t83089tu045jt054050j")
 	v, err := m.ComputeMAC(data[:])
 	require.NoError(t, err, "should compute mac")
+	v = v[:length]
 
-	bidx, err := NewBIDX(handle, 0)
+	bidx, err := NewBIDX(handle, length)
 	require.NoError(t, err)
 	v1, err := bidx.ComputePrimary(data)
 	require.NoError(t, err)
 	assert.Equal(t, v, v1)
+	assert.Len(t, v1, length)
 
-	hid, err = mgr.Add(mac.HMACSHA256Tag128KeyTemplate())
+	hid, err = mgr.Add(mac.HMACSHA256Tag256KeyTemplate())
 	require.NoError(t, err, "should add new mac handle")
 	err = mgr.SetPrimary(hid)
 	require.NoError(t, err, "should set new primary handle")
 	handle, err = mgr.Handle()
 	require.NoError(t, err, "should obtain new mac handle")
 
-	for i := 0; i < 10; i++ {
-		vs, err := GetBlindIdxs(handle, data[:], len(v))
-		require.NoError(t, err, "should compute multiple mac")
-		vs1, err := bidx.ComputeAll(data)
-		require.NoError(t, err, "should compute multiple mac")
+	for i := 0; i < 3; i++ {
+		bidx, err := NewBIDX(handle, length)
+		require.NoError(t, err)
 
-		assert.Contains(t, vs, v, "should contain previous mac")
-		assert.Contains(t, vs1, v, "should contain previous mac")
+		v2, err := bidx.ComputePrimary(data)
+		require.NoError(t, err, "should compute primary mac")
+		assert.NotEqual(t, v1, v2)
+		assert.Len(t, v2, length)
+
+		vs, err := bidx.ComputeAll(data)
+		require.NoError(t, err, "should compute multiple mac")
+		assert.Contains(t, vs, v1, "should contain previous mac")
+		assert.Contains(t, vs, v2, "should contain new mac")
+
+		vsold, err := GetBlindIdxs(handle, data[:], len(v))
+		require.NoError(t, err, "should compute multiple mac")
+		assert.Contains(t, vsold, v1, "should contain previous mac")
+		assert.Contains(t, vsold, v2, "should contain new mac")
 	}
 }
 
